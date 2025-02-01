d918d2fb90a3c4d6872fd104b0bcaa500f7ae2b1
kashishksy <kashishksy2112@gmail.com>
2025-02-01 12:17:43 -0500

    removing comments / unused code from main activity

--- app/src/main/java/com/kashish/advancedandroid_assignment1/MainActivity.kt
+++ app/src/main/java/com/kashish/advancedandroid_assignment1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -27,20 +28,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import com.kashish.advancedandroid_assignment1.viewmodel.ViewModelFactory
+import com.kashish.advancedandroid_assignment1.datastore.DataStoreManager
 import com.kashish.advancedandroid_assignment1.ui.theme.AdvancedAndroid_assignment1Theme
+import com.kashish.advancedandroid_assignment1.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel> {
+        ViewModelFactory(DataStoreManager(applicationContext))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AdvancedAndroid_assignment1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-                    StudentInfoBox()
+                    MainScreen(
+                        viewModel = viewModel,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -48,60 +56,3 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Composable
-fun MyCard2() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(16.dp) // Add margin around the entire Row
-    ) {
-        Column {
-            Text(
-                text = "Kashish Yadav",
-                modifier = Modifier.padding(bottom = 8.dp) // Add bottom margin between the two Text components
-            )
-            Text(
-                text = "Student ID: 301485842",
-                modifier = Modifier.padding(top = 8.dp) // Add top margin for spacing consistency
-            )
-        }
-    }
-}
-
-@Composable
-fun StudentInfoBox(modifier: Modifier = Modifier) {
-    var colorState by remember { mutableStateOf(true) }
-    var bgColor by remember { mutableStateOf(Color.White) }
-
-    //val fuchsia = Color(0xFFFF00FF) // Vibrant fuchsia
-    val lightFuchsia = Color(0xFFF694D0) // Muted fuchsia for softer contrast
-    val clickHandler = {
-        colorState = !colorState
-        bgColor = if (colorState) Color.White else lightFuchsia
-    }
-    Box(
-        modifier = Modifier
-            .fillMaxSize() // Make the parent Box fill the screen
-    ) {
-        Box(
-            Modifier
-                .fillMaxWidth() // Make the Box take the full width
-                .height(150.dp) // Adjust the height as needed
-                .padding(16.dp) // Add margin around the Box (outer space)
-                .align(Alignment.BottomCenter) // Align it at the bottom center of the screen
-                .clickable { clickHandler() }
-                .background(bgColor, shape = RoundedCornerShape(15.dp)) // Add rounded corners to the background
-                .border(2.dp, Color.Black, RoundedCornerShape(16.dp)) // Add a rounded border
-            //.background(bgColor)
-        ) {
-            MyCard2()
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun StudentInfoBoxPreview() {
-    AdvancedAndroid_assignment1Theme {
-        StudentInfoBox()
-    }
-}
